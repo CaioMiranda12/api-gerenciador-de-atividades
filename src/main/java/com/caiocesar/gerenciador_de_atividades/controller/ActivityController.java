3b2ab177e35d9ee61a36c811af13ea87e39f6cc3
CaioMiranda12 <caioclm04@gmail.com>
2025-10-14 12:20:46 -0300

    feat: add ActivityController

---
+++ src/main/java/com/caiocesar/gerenciador_de_atividades/controller/ActivityController.java
@@ -0,0 +1,41 @@
+package com.caiocesar.gerenciador_de_atividades.controller;
+
+import com.caiocesar.gerenciador_de_atividades.dto.activity.ActivityDTO;
+import com.caiocesar.gerenciador_de_atividades.services.ActivityService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/activities")
+@RequiredArgsConstructor
+public class ActivityController {
+
+    private final ActivityService activityService;
+
+    @PostMapping
+    public ResponseEntity<ActivityDTO> createActivity(@RequestBody ActivityDTO activityDTO){
+        ActivityDTO createdActivity = activityService.createActivity(activityDTO);
+        return ResponseEntity.status(201).body(createdActivity);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ActivityDTO>> getAllActivities(){
+        List<ActivityDTO> activities = activityService.getAllActivities();
+        return ResponseEntity.ok(activities);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ActivityDTO> updateActivity(@PathVariable Long id, @RequestBody ActivityDTO activityDTO){
+        ActivityDTO updatedActivity = activityService.updateActivity(id, activityDTO);
+        return ResponseEntity.ok(updatedActivity);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteActivity(@PathVariable Long id){
+        activityService.deleteActivity(id);
+        return ResponseEntity.noContent().build();
+    }
+}
